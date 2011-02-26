fc7eadf3a08fbbaa943f7b8ba736736bdc230eb9
Hong Minhee <minhee@dahlia.kr>
2011-02-26 14:23:21 +0900

    Separated the configuration from the application. Styled Atom feed. List of best challenge comics.

--- navercomicfeed/app.py
+++ navercomicfeed/app.py
@@ -2,14 +2,22 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
+import os
+import os.path
+import urllib2
+import contextlib
+import hmac
+import hashlib
 from flask import *
 from flaskext.cache import Cache
 import lxml.html
 import werkzeug.urls
+import sqlalchemy
 from navercomicfeed.comic import *
 
 
 WEBTOON_LIST_URL = 'http://comic.naver.com/webtoon/creation.nhn?view=list'
+BESTCHALLENGE_LIST_URL = 'http://comic.naver.com/genre/bestChallenge.nhn'
 URL_TYPES = {
     'webtoon': 'http://comic.naver.com/webtoon/list.nhn?titleId={0}',
     'challenge': 'http://comic.naver.com/challenge/list.nhn?titleId={0}',
@@ -18,7 +26,20 @@ URL_TYPES = {
 
 
 app = Flask(__name__)
+app.config['IMGPROXY_URL'] = 'http://imgproxy.dahlia.kr/image.php'
+try:
+    config_env = os.environ['NAVERCOMICFEED_CONFIG']
+    app.config.from_pyfile(os.path.abspath(config_env))
+    del config_env
+except (KeyError, IOError):
+    pass
 cache = Cache(app)
+
+
+@app.before_request
+def before_request():
+    g.engine = sqlalchemy.create_engine(app.config['DATABASE'])
+    Session.configure(bind=g.engine)
 
 
 @app.route('/')
@@ -41,13 +62,37 @@ def webtoon_list():
     return render_template('webtoon_list.html', comics=comics)
 
 
+def bestchallenge_comics():
+    page = 1
+    url = BESTCHALLENGE_LIST_URL + '?page={0}'
+    prev_title_ids = set()
+    while True:
+        html = lxml.html.parse(url.format(page))
+        links = html.xpath('//*[@id="content"]//table[@class="challengeList"]'
+                           '//td/*[@class="fl"]/a')
+        title_ids = set()
+        for a in links:
+            href = a.attrib['href']
+            query = href[href.index('?') + 1:]
+            title_id = int(werkzeug.urls.url_decode(query)['titleId'])
+            yield title_id, a.xpath('./img/@title')[0]
+            title_ids.add(title_id)
+        if title_ids.issubset(prev_title_ids):
+            break
+        prev_title_ids.update(title_ids)
+        page += 1
+
+
+@app.route('/bestchallenge')
+@cache.cached(timeout=3600 * 6)
+def bestchallenge_list():
+    comics = dict(bestchallenge_comics())
+    return render_template('bestchallenge_list.html', comics=comics)
+
+
 @app.route('/<any(webtoon,bestchallenge,challenge):type>/<int:title_id>.xml')
 @cache.cached(timeout=120)
 def feed(type, title_id):
-    from sqlalchemy import create_engine
-    engine = create_engine('sqlite:///db.sqlite')
-    Base.metadata.create_all(engine)
-    Session.configure(bind=engine)
     session = Session()
     url = URL_TYPES[type].format(title_id)
     title = Title(url, session)
@@ -58,8 +103,66 @@ def feed(type, title_id):
         pass
     else:
         title = title[:limit]
-    xml = render_template('feed.xml', title=title)
-    return Response(response=xml, content_type='application/atom+xml')
+    try:
+        imgproxy_key = app.config['IMGPROXY_KEY']
+        imgproxy_secret_key = app.config['IMGPROXY_SECRET_KEY']
+        imgproxy_url = app.config['IMGPROXY_URL']
+    except KeyError:
+        def proxy_fn(url):
+            return url_for('image_proxy', url=url, _external=True)
+    else:
+        def imgproxy_sig(url):
+            hash = hmac.new(imgproxy_secret_key, url, hashlib.sha256)
+            return hash.hexdigest()
+        def proxy_fn(url):
+            query = {'url': url, 'key': imgproxy_key, 'sig': imgproxy_sig(url)}
+            return imgproxy_url + '?' + werkzeug.urls.url_encode(query)
+    xml = render_template('feed.xml', title=title, proxy_url_for=proxy_fn)
+    types = ['text/xml', 'application/atom+xml']
+    type = request.accept_mimetypes.best_match(types)
+    return Response(response=xml, content_type=type)
+
+
+@app.route('/img')
+@cache.cached(timeout=3600 * 6)
+def image_proxy():
+    imgproxy_cfgs = 'URL', 'KEY', 'SECRET_KEY'
+    if all('IMGPROXY_' + c in app.config for c in imgproxy_cfgs):
+        abort(403)
+    def fetch():
+        url = request.values['url']
+        with contextlib.closing(urllib2.urlopen(url)) as f:
+            yield f.info()['Content-Type']
+            while True:
+                buffer = f.read(4096)
+                if buffer:
+                    yield buffer
+                else:
+                    break
+    it = fetch()
+    content_type = it.next()
+    return Response(response=it, content_type=content_type)
+
+
+@app.route('/admin/setup')
+def admin_setup():
+    try:
+        admin_pred_fn = app.config['ADMIN_PRED_FN']
+    except KeyError:
+        abort(403)
+    else:
+        auth = request.authorization
+        if auth:
+            user = auth.username
+            password = auth.password
+        else:
+            user = password = None
+        if not admin_pred_fn(user, password):
+            realm = 'NaverComicFeed Administrator'
+            headers = {'WWW-Authenticate': 'Basic realm="{0}"'.format(realm)}
+            return Response(realm, 401, headers)
+    Base.metadata.create_all(g.engine)
+    return 'Successfully tables created.'
 
 
 if __name__ == '__main__':
---
+++ navercomicfeed/static/feed.xsl
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- 네이버 웹툰 RSS 스타일 -->
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:atom="http://www.w3.org/2005/Atom"
+                xmlns:dc="http://purl.org/dc/elements/1.1/">
+  <xsl:output method="html" encoding="utf-8"/>
+	
+  <xsl:template match="/">
+    <html>
+      <head>
+        <title>
+          <xsl:apply-templates select="/atom:feed/atom:title/text()" />
+        </title>
+        <link rel="stylesheet"
+              href="{normalize-space(substring-after(
+                     /atom:feed/atom:generator, '|'))}"
+              type="text/css" />
+      </head>
+      <body>
+        <header>
+          <h1>
+            <a href="{/atom:feed/atom:link[contains(concat(' ', @rel, ' '),
+                                                    ' alternate ')]/@href}"
+               rel="alternate">
+              <xsl:value-of select="/atom:feed/atom:title" />
+            </a>
+          </h1>
+          <p><xsl:value-of select="/atom:feed/atom:subtitle" /></p>
+        </header>
+        <div class="articles">
+          <xsl:apply-templates select="/atom:feed/atom:entry">
+            <xsl:sort select="atom:published" order="descending" />
+          </xsl:apply-templates>
+        </div>
+        <footer>
+          <p>이 URL은 <a href="{atom:feed/atom:generator/@uri}">
+             네이버 웹툰 RSS</a>에서 제공하는 Atom 형식의 XML 문서입니다.</p>
+        </footer>
+      </body>
+    </html>
+  </xsl:template>
+  
+  <xsl:template match="atom:entry">
+    <article>
+      <time datetime="{atom:updated}">
+        <span class="date">
+          <xsl:value-of select="substring-before(atom:updated, 'T')" />
+        </span>
+        <span class="time">
+          <xsl:value-of select="substring(atom:updated, 12, 8)" />
+        </span>
+      </time>
+      <h2>
+        <a href="{atom:link/@href}"><xsl:value-of select="atom:title" /></a>
+      </h2>
+      <div class="images">
+        <xsl:apply-templates select="atom:link[contains(concat(' ', @rel, ' '),
+                                                        ' enclosure ')]" />
+      </div>
+      <p><xsl:value-of select="atom:summary" /></p>
+    </article>
+  </xsl:template>
+
+  <xsl:template match="atom:link">
+    <div><img src="{@href}" alt="" /></div>
+  </xsl:template>
+</xsl:stylesheet>
+
