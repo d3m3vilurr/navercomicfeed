49c0cf5e304094beca407100377bd87f2e5cf6e5
Hong Minhee <minhee@dahlia.kr>
2011-02-28 18:53:34 +0900

    Cache backend for Redis.

---
+++ navercomicfeed/cache.py
@@ -0,0 +1,103 @@
+""":mod:`naverwebcomic.cache` --- Custom cache backends
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+It implements custom cache backends powered by :mod:`werkzeug.contrib.cache`
+and :mod:`flaskext.cache`.
+
+Currently, it implements a Redis backend only.
+
+"""
+import itertools
+import json
+import flask
+import flaskext.cache
+import werkzeug.contrib.cache
+
+
+class RedisCache(werkzeug.contrib.cache.BaseCache):
+    """A cache that uses Redis as backend.
+
+    :param redis_client: a Redis client
+    :type redis_client: :class:`redis.Redis`
+    :param default_timeout: the default timeout that is used if no timeout
+                            is specified on :meth:`set()`.
+
+    .. seealso:: Package redis_
+
+    .. _redis: https://github.com/andymccurdy/redis-py
+
+    """
+
+    def __init__(self, redis_client, default_timeout=300):
+        super(RedisCache, self).__init__(default_timeout=default_timeout)
+        self.redis = redis_client
+
+    def add(key, value, timeout=None):
+        pipe = self.redis.pipeline()
+        value = json.dumps(value)
+        pipe.setnx(key, value).expire(key, timeout or self.default_timeout)
+        pipe.execute()
+
+    def clear(self):
+        self.redis.flushdb()
+
+    def dec(self, key, delta=1):
+        self.redis.decr(key, amount=delta)
+
+    def delete(self, key):
+        self.redis.delete(key)
+
+    def delete_many(self, *keys):
+        self.redis.delete(*keys)
+
+    def get(self, key):
+        v = self.redis.get(key)
+        return v and json.loads(v)
+
+    def get_dict(self, *keys):
+        values = map(json.loads, self.redis.mget(keys))
+        return dict(itertools.izip(keys, values))
+
+    def get_many(self, *keys):
+        return map(json.loads, self.redis.mget(keys))
+
+    def inc(self, key, delta=1):
+        self.redis.incr(key, amount=delta)
+
+    def set(self, key, value, timeout=None):
+        timeout = timeout or self.default_timeout
+        self.redis.setex(key, json.dumps(value), timeout)
+
+    def set_many(self, mapping, timeout=None):
+        pipe = self.redis.pipeline()
+        mappding = dict((k, json.dumps(v)) for k, v in mapping.iteritems())
+        pipe.mset(mapping).expire(key, timeout or self.default_timeout)
+        pipe.execute()
+
+
+def redis(app, args, kwargs):
+    """A Redis backend for Flask-Cache. Set ``CACHE_TYPE`` to
+    ``'styleshare.cache.redis'`` in your Flask configuration.
+
+    There are additional configuration values for it:
+
+    - ``CACHE_REDIS_DB`` (required)
+    - ``CACHE_REDIS_HOST``
+    - ``CACHE_REDIS_PORT``
+    - ``CACHE_REDIS_PASSWORD``
+    - ``CACHE_REDIS_SOCKET_TIMEOUT``
+
+    .. warning:: It requires redis_ package (known as **redis-py** also).
+
+    .. redis_: https://github.com/andymccurdy/redis-py
+
+    """
+    from redis import Redis
+    client = Redis(db=app.config['CACHE_REDIS_DB'],
+                   host=app.config.get('CACHE_REDIS_HOST'),
+                   port=app.config.get('CACHE_REDIS_PORT'),
+                   password=app.config.get('CACHE_REDIS_PASSWORD'),
+                   socket_timeout=app.config.get('CACHE_REDIS_SOCKET_TIMEOUT'))
+    args.append(client)
+    return RedisCache(*args, **kwargs)
+
